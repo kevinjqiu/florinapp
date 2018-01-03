a9af6266a66be5b92cb3a4b5534bd7df242f78c3
Kevin Jing Qiu <kevin@idempotent.ca>
2018-01-03 00:22:43 -0500

    Style change

--- src/components/Sidebar/_nav.js
+++ src/components/Sidebar/_nav.js
@@ -2,52 +2,52 @@
 export default {
   items: [
     {
-      name: 'Dashboard',
-      url: '/dashboard',
-      icon: 'icon-grid',
+      name: "Dashboard",
+      url: "/dashboard",
+      icon: "icon-grid"
     },
     {
-      name: 'Accounts',
-      url: '/accounts',
-      icon: 'icon-wallet',
+      name: "Accounts",
+      url: "/accounts",
+      icon: "icon-wallet"
     },
     {
-      name: 'Transactions',
-      url: '/transactions',
-      icon: 'icon-book-open',
+      name: "Transactions",
+      url: "/transactions",
+      icon: "icon-book-open"
     },
     {
-      name: 'Budgets',
-      url: '/budgets',
-      icon: 'icon-calculator',
+      name: "Budgets",
+      url: "/budgets",
+      icon: "icon-calculator"
     },
     {
       divider: true
     },
     {
-      name: 'Settings',
-      url: '/settings',
-      icon: 'icon-options',
+      name: "Settings",
+      url: "/settings",
+      icon: "icon-options",
       children: [
         {
-          name: 'General',
-          url: '/settings/general',
-          icon: 'icon-settings'
+          name: "General",
+          url: "/settings/general",
+          icon: "icon-settings"
         },
         {
-          name: 'Categories',
-          url: '/settings/categories',
-          icon: 'icon-pie-chart'
+          name: "Categories",
+          url: "/settings/categories",
+          icon: "icon-pie-chart"
         },
         {
-          name: 'Tags',
-          url: '/settings/tags',
-          icon: 'icon-tag'
+          name: "Tags",
+          url: "/settings/tags",
+          icon: "icon-tag"
         },
         {
-          name: 'Sync',
-          url: '/settings/sync',
-          icon: 'icon-refresh'
+          name: "Sync",
+          url: "/settings/sync",
+          icon: "icon-refresh"
         }
       ]
     }
