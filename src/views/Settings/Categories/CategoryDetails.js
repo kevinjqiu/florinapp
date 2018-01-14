f9a12057547d0a50008f77aa6c954a270569c70d
Kevin Jing Qiu <kevin@idempotent.ca>
2018-01-14 18:38:53 -0500

    Add category detail page

---
+++ src/views/Settings/Categories/CategoryDetails.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Row, Col, Container } from "reactstrap";
+import { connect } from "react-redux";
+import CategoryForm from "./CategoryForm";
+import { reduxForm } from "redux-form";
+import * as actions from "../../../actions";
+import Category from "../../../models/Category";
+
+class CategoryDetails extends Component {
+
+  render() {
+    const { categoryId } = this.props.match.params;
+    const { categories, updateCategory } = this.props;
+
+    const category = categories.filter(c => c._id === categoryId)[0];
+
+    if (!category) {
+      return <h3>No such category found</h3>
+    }
+
+    const EditCategoryForm = reduxForm({
+        initialValues: category,
+        form: "editCategory"
+      })(CategoryForm);
+
+    return <Container fluid>
+        <Row>
+          <Col xs="12" lg="12">
+            <h3>Category Details</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="12" lg="12">
+            <EditCategoryForm editMode onSubmit={props => {
+              updateCategory(categoryId, new Category(props));
+            }} />
+          </Col>
+        </Row>
+      </Container>;
+  }
+}
+
+const mapStateToProps = ({ categories }) => {
+  return { categories: categories.categories };
+}
+
+export default connect(mapStateToProps, actions)(CategoryDetails);
