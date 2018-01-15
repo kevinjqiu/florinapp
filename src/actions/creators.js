1d58d052ad5ca2d4fa1773ea5f05b0d17f8661d5
Kevin Jing Qiu <kevin@idempotent.ca>
2018-01-14 21:15:43 -0500

    Action and action creators

--- src/actions/creators.js
+++ src/actions/creators.js
@@ -203,6 +203,20 @@ export const updateCategorySucceeded = (category: Category) => {
 
 export const updateCategoryFailed = actionFailed(actionTypes.UPDATE_CATEGORY_FAILED);
 
+export const deleteCategoryRequested = () => {
+  return {
+    type: actionTypes.DELETE_CATEGORY_REQUESTED
+  }
+}
+
+export const deleteCategorySucceeded = () => {
+  return {
+    type: actionTypes.DELETE_CATEGORY_SUCCEEDED,
+  }
+}
+
+export const deleteCategoryFailed = actionFailed(actionTypes.DELETE_CATEGORY_FAILED);
+
 export const updateTransactionCategoryRequested = (
   transactionId: string,
   categoryId: string
