30782c3329e9959cb7d9ed42aa0757cbaaffe07c
Kevin Jing Qiu <kevin@idempotent.ca>
2018-01-14 21:08:15 -0500

    Service code to delete category

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,6 +1,7 @@
 import Category from "../models/Category";
 import db from "../db";
 import { MAX_NUMBER } from "./const";
+import * as transactionService from "../services/transactionService";
 
 export const fetch = async (): Promise<Array<Category>> => {
   const response = await db.find({
@@ -36,4 +37,36 @@ export const update = async (categoryId: string, categoryData: Category): Promis
   await db.put(category);
 
   return new Category(category);  // TODO: fetchById?
+}
+
+export const del = async (categoryId: string) => {
+  const doc = await db.get(categoryId);
+  await db.remove(doc);
+  const transactions = await transactionService.fetch({
+    orderBy: ["date", "asc"],
+    pagination: {
+      perPage: MAX_NUMBER,
+      page: 1
+    },
+    filters: { categoryId }
+  });
+
+  await Promise.all(transactions.result.map(t => {
+    return transactionService.updateCategory(t._id, undefined);
+  }));
+
+  const response = await db.find({
+    selector: {
+      "metadata.type": "Category",
+      parent: categoryId
+    },
+    limit: MAX_NUMBER
+  });
+
+  await Promise.all(response.docs.map(async doc => {
+    await db.put({
+      ...doc,
+      parent: undefined
+    });
+  }));
 }
--- src/services/categoryService.test.js
+++ src/services/categoryService.test.js
@@ -3,6 +3,7 @@ import db from "../db";
 import reset from "../db/reset";
 import seed from "../db/seed";
 import Category from "../models/Category";
+import Transaction from "../models/Transaction";
 
 describe("categoryService", () => {
   beforeEach(async () => {
@@ -19,25 +20,25 @@ describe("categoryService", () => {
 
   describe("categoryService.create", () => {
     it("should create a top-level category with id being lowercased name", async () => {
-      const category = await categoryService.create({
+      const category = await categoryService.create(new Category({
         name: "Awesome Category",
         parent: null
-      });
+      }));
 
       expect(category._id).toEqual("awesomecategory");
     });
 
     it("should create a sub-category with id being the parent hyphen subcategory name", async () => {
-      await categoryService.create({
+      await categoryService.create(new Category({
         _id: "awesomecategory",
         name: "Awesome Category",
         parent: null
-      });
+      }));
 
-      const subcategory = await categoryService.create({
+      const subcategory = await categoryService.create(new Category({
         name: "Sub awesome category",
         parent: "awesomecategory"
-      });
+      }));
 
       expect(subcategory._id).toEqual("awesomecategory-subawesomecategory");
     });
@@ -45,14 +46,72 @@ describe("categoryService", () => {
 
   describe("categoryService.update", () => {
     it("should update the existing category", async () => {
-      await categoryService.create({
+      await categoryService.create(new Category({
         _id: "awesomecategory",
         name: "Awesome Category",
         parent: null
-      });
+      }));
       const category = await categoryService.update("awesomecategory", {name: "AWESOME"})
       expect(category._id).toEqual("awesomecategory");
       expect(category.name).toEqual("AWESOME");
     });
+  });
+
+  describe("categoryService.del", () => {
+    it("should delete the category from the database", async () => {
+      await categoryService.create(new Category({
+        _id: "awesomecategory",
+        name: "Awesome Category",
+        parent: null
+      }));
+
+      await categoryService.del("awesomecategory");
+      try {
+        await db.get("awesomecategory");
+        fail("Shouldn't get here");
+      } catch (error) {
+        expect(error.status).toEqual(404);
+      }
+    });
+
+    it("should reset the transactions previously categorized", async () => {
+      await categoryService.create(new Category({
+        _id: "awesomecategory",
+        name: "Awesome Category",
+        parent: null
+      }));
+      await db.post(new Transaction({_id: "txn1", date: "2015-01-01", categoryId: "awesomecategory"}));
+      await db.post(new Transaction({_id: "txn2", date: "1992-10-01", categoryId: "awesomecategory"}));
+      await db.post(new Transaction({_id: "txn3", date: "1981-11-06", categoryId: "awesomecategory"}));
+
+      await categoryService.del("awesomecategory");
+
+      expect((await db.get("txn1")).categoryId).toBe(undefined);
+      expect((await db.get("txn2")).categoryId).toBe(undefined);
+      expect((await db.get("txn3")).categoryId).toBe(undefined);
+    });
+
+    it("should reset the parent of sub-categories", async () => {
+      await categoryService.create(new Category({
+        _id: "awesomecategory",
+        name: "Awesome Category",
+        parent: null
+      }));
+      await categoryService.create(new Category({
+        _id: "cat-1",
+        name: "Cat 1",
+        parent: "awesomecategory"
+      }));
+      await categoryService.create(new Category({
+        _id: "cat-2",
+        name: "Cat 2",
+        parent: "awesomecategory"
+      }));
+
+      await categoryService.del("awesomecategory");
+
+      expect((await db.get("cat-1")).parent).toBe(undefined);
+      expect((await db.get("cat-2")).parent).toBe(undefined);
+    })
   });
 });
